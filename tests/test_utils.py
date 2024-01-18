5c31c2faef96d00b27507cbb92fd6ad105da5ad5
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2024-01-18 17:55:33 +0000

    Update VERSION and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,22 +9,17 @@ from instance_billing_flavor_check import utils
 FAKE_PROXY = {'http_proxy': 'foo', 'https_proxy': 'bar', 'no_proxy': 'foobar'}
 
 @patch.dict(os.environ, FAKE_PROXY, clear=True)
-def test_get_proxies(caplog):
+def test_get_proxies():
     """Test proxy settings are the same as environment, if any."""
-    with caplog.at_level(logging.INFO):
-        assert utils._get_proxies() is None
-    expected_message = 'Using proxy settings present on the environment.'
-    assert expected_message in caplog.text
+    assert not utils._get_proxies()
 
 
-def test_get_proxies_no_proxy_env_no_proxy_file(caplog):
+def test_get_proxies_no_proxy_env_no_proxy_file():
     """Test proxy value is the same as set on the proxy file."""
     with patch('builtins.open', create=True) as mock_open:
         mock_open.side_effect = FileNotFoundError('oh no')
-        with caplog.at_level(logging.INFO):
-            assert utils._get_proxies() is None
-        expected_message = 'No proxy config file found on /etc/sysconfig/proxy'
-        assert expected_message  in caplog.text
+        assert not utils._get_proxies()
+
 
 
 def test_get_proxies_no_proxy_env_proxy_file(caplog):
