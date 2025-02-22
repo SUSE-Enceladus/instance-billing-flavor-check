4692bc3cdbbe63a60285654c0dcb33e20046dee4
Robert Schweikert <rjschwei@suse.com>
2025-02-22 09:10:27 -0500

    Make the function that checks the flavor reusable
    Previously the function check_payg_byos which is the entry point to determine
    the instance flavor exited using the sys.exit() Python function. This made
    the function not reusable in other code bases that may need to determine the
    flavor. Change the behavior of the function to return the flavor indicator
    as a string and return a code indicating the flavor.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ from unittest import mock
 from unittest.mock import patch
 from instance_billing_flavor_check import utils
 
+CACHE_FILE_PATH = '/tmp/instance-billing-flavor-check'
 FAKE_PROXY = {'http_proxy': 'foo', 'https_proxy': 'bar', 'no_proxy': 'foobar'}
 
 @patch.dict(os.environ, FAKE_PROXY, clear=True)
@@ -37,3 +38,133 @@ def test_get_proxies_no_proxy_env_proxy_file(caplog):
                 'https_proxy': 'https://foo.com',
                 'no_proxy': 'localhost, 127.0.0.1'
             }
+
+
+def test_check_payg_byos_no_network():
+    """Check proper return when we do not have network access"""
+    utils.has_ipv4_access = _no_ip
+    utils.has_ipv6_access = _no_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    result = utils.check_payg_byos()
+    assert(result[0] == 'BYOS')
+    assert(result[1] == 12)
+    assert(open(CACHE_FILE_PATH).read() == 'BYOS')
+    os.unlink(CACHE_FILE_PATH)
+
+
+@patch('instance_billing_flavor_check.utils.get_identifier')
+@patch('instance_billing_flavor_check.utils.get_metadata')
+def test_check_payg_byos_no_instance_data(
+        mock_metadata, mock_identifier, caplog
+):
+    """Check proper return when there is no instance information"""
+    utils.has_ipv4_access = _has_ip
+    utils.has_ipv6_access = _no_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    mock_identifier.return_value = False
+    mock_metadata.return_value = False
+    result = utils.check_payg_byos()
+    assert(result[0] == 'BYOS')
+    assert(result[1] == 12)
+    assert(open(CACHE_FILE_PATH).read() == 'BYOS')
+    assert('No instance metadata and identifier' in caplog.text)
+    os.unlink(CACHE_FILE_PATH)
+
+
+@patch('instance_billing_flavor_check.utils.get_identifier')
+@patch('instance_billing_flavor_check.utils.get_metadata')
+@patch('instance_billing_flavor_check.utils.get_rmt_ip_addr')
+def test_check_payg_byos_no_server_ip(
+        mock_rmt_ip, mock_metadata, mock_identifier, caplog
+):
+    """Check proper return when the update server claims to have no IP"""
+    utils.has_ipv4_access = _no_ip
+    utils.has_ipv6_access = _has_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    mock_identifier.return_value = True
+    mock_metadata.return_value = True
+    mock_rmt_ip.return_value = None
+    result = utils.check_payg_byos()
+    assert(result[0] == 'BYOS')
+    assert(result[1] == 12)
+    assert(open(CACHE_FILE_PATH).read() == 'BYOS')
+    assert('Instance can be either' in caplog.text)
+    os.unlink(CACHE_FILE_PATH)
+
+
+@patch('instance_billing_flavor_check.utils.get_identifier')
+@patch('instance_billing_flavor_check.utils.get_metadata')
+@patch('instance_billing_flavor_check.utils.get_rmt_ip_addr')
+@patch('instance_billing_flavor_check.utils.make_request')
+def test_check_payg_byos_is_payg(
+        mock_request, mock_rmt_ip, mock_metadata, mock_identifier
+):
+    """Check proper return based on the update server status"""
+    utils.has_ipv4_access = _no_ip
+    utils.has_ipv6_access = _has_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    mock_identifier.return_value = True
+    mock_metadata.return_value = True
+    mock_rmt_ip.return_value = ['1.1.1.1']
+    mock_request.return_value = 'PAYG'
+    result = utils.check_payg_byos()
+    assert(result[0] == 'PAYG')
+    assert(result[1] == 10)
+    assert(open(CACHE_FILE_PATH).read() == 'PAYG')
+    os.unlink(CACHE_FILE_PATH)
+
+
+@patch('instance_billing_flavor_check.utils.get_identifier')
+@patch('instance_billing_flavor_check.utils.get_metadata')
+@patch('instance_billing_flavor_check.utils.get_rmt_ip_addr')
+@patch('instance_billing_flavor_check.utils.make_request')
+def test_check_payg_byos_is_byos(
+        mock_request, mock_rmt_ip, mock_metadata, mock_identifier
+):
+    """Check proper return based on the update server status"""
+    utils.has_ipv4_access = _no_ip
+    utils.has_ipv6_access = _has_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    mock_identifier.return_value = True
+    mock_metadata.return_value = True
+    mock_rmt_ip.return_value = ['1.1.1.1']
+    mock_request.return_value = 'BYOS'
+    result = utils.check_payg_byos()
+    assert(result[0] == 'BYOS')
+    assert(result[1] == 11)
+    assert(open(CACHE_FILE_PATH).read() == 'BYOS')
+    os.unlink(CACHE_FILE_PATH)
+
+
+@patch('instance_billing_flavor_check.utils.get_identifier')
+@patch('instance_billing_flavor_check.utils.get_metadata')
+@patch('instance_billing_flavor_check.utils.get_rmt_ip_addr')
+@patch('instance_billing_flavor_check.utils.make_request')
+def test_check_payg_byos_use_cache(
+        mock_request, mock_rmt_ip, mock_metadata, mock_identifier
+):
+    """Check proper return based on the update server status"""
+    utils.has_ipv4_access = _no_ip
+    utils.has_ipv6_access = _has_ip
+    utils.CACHE_FILE_PATH = CACHE_FILE_PATH
+    cf = open(CACHE_FILE_PATH, 'w')
+    cf.write('PAYG')
+    cf.close()
+    mock_identifier.return_value = True
+    mock_metadata.return_value = True
+    mock_rmt_ip.return_value = ['1.1.1.1']
+    mock_request.return_value = 'PAYG'
+    result = utils.check_payg_byos()
+    assert(result[0] == 'PAYG')
+    assert(result[1] == 10)
+    assert(open(CACHE_FILE_PATH).read() == 'PAYG')
+    os.unlink(CACHE_FILE_PATH)
+
+    
+## Test helpers
+def _no_ip():
+    return False
+
+
+def _has_ip():
+    return True
